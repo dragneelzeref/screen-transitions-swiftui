ea1fe925d420198f648a8a3270df1b85a916a61f
Ravi Pansuriya <pansuriyaravi.rc@gmail.com>
2022-03-23 21:53:00 +0530

    Initial Commit

---
+++ ScreenTransition/ScreenTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTransitionApp.swift
+//  ScreenTransition
+//
+//  Created by Ravi on 23/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
